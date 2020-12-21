e377037500e2542884c48ef955fd8d78512ad32b
alex.gharbi@hotmail.com <alex.gharbi@hotmail.com>
2020-12-21 18:55:30 +0100

    form

--- DetectionFilterForm/Form1.Designer.cs
+++ DetectionFilterForm/Form1.Designer.cs
@@ -28,21 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.btnNightFilter = new System.Windows.Forms.Button();
+            this.cmbEdgeDetection = new System.Windows.Forms.ComboBox();
+            this.btnSaveNewImage = new System.Windows.Forms.Button();
+            this.picPreview = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(485, 607);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 45);
+            this.button2.TabIndex = 43;
+            this.button2.Text = "No filter";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 678);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 40);
+            this.button1.TabIndex = 42;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(62, 607);
+            this.button12.Margin = new System.Windows.Forms.Padding(4);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(177, 45);
+            this.button12.TabIndex = 41;
+            this.button12.Text = "Hell Filter";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // btnNightFilter
+            // 
+            this.btnNightFilter.Location = new System.Drawing.Point(279, 607);
+            this.btnNightFilter.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNightFilter.Name = "btnNightFilter";
+            this.btnNightFilter.Size = new System.Drawing.Size(177, 45);
+            this.btnNightFilter.TabIndex = 40;
+            this.btnNightFilter.Text = "Night Filter";
+            this.btnNightFilter.UseVisualStyleBackColor = true;
+            // 
+            // cmbEdgeDetection
+            // 
+            this.cmbEdgeDetection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEdgeDetection.Enabled = false;
+            this.cmbEdgeDetection.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbEdgeDetection.FormattingEnabled = true;
+            this.cmbEdgeDetection.Items.AddRange(new object[] {
+            "None",
+            "Laplacian 3x3",
+            "Laplacian 3x3 Grayscale",
+            "Laplacian 5x5",
+            "Laplacian 5x5 Grayscale",
+            "Laplacian of Gaussian",
+            "Laplacian 3x3 of Gaussian 3x3",
+            "Laplacian 3x3 of Gaussian 5x5 - 1",
+            "Laplacian 3x3 of Gaussian 5x5 - 2",
+            "Laplacian 5x5 of Gaussian 3x3",
+            "Laplacian 5x5 of Gaussian 5x5 - 1",
+            "Laplacian 5x5 of Gaussian 5x5 - 2",
+            "Sobel 3x3",
+            "Sobel 3x3 Grayscale",
+            "Prewitt",
+            "Prewitt Grayscale",
+            "Kirsch",
+            "Kirsch Grayscale"});
+            this.cmbEdgeDetection.Location = new System.Drawing.Point(220, 681);
+            this.cmbEdgeDetection.Name = "cmbEdgeDetection";
+            this.cmbEdgeDetection.Size = new System.Drawing.Size(288, 37);
+            this.cmbEdgeDetection.TabIndex = 39;
+            // 
+            // btnSaveNewImage
+            // 
+            this.btnSaveNewImage.Enabled = false;
+            this.btnSaveNewImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveNewImage.Location = new System.Drawing.Point(514, 675);
+            this.btnSaveNewImage.Name = "btnSaveNewImage";
+            this.btnSaveNewImage.Size = new System.Drawing.Size(183, 46);
+            this.btnSaveNewImage.TabIndex = 38;
+            this.btnSaveNewImage.Text = "Save Image";
+            this.btnSaveNewImage.UseVisualStyleBackColor = true;
+            // 
+            // picPreview
+            // 
+            this.picPreview.BackColor = System.Drawing.Color.Silver;
+            this.picPreview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.picPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picPreview.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.picPreview.Location = new System.Drawing.Point(62, 24);
+            this.picPreview.Name = "picPreview";
+            this.picPreview.Size = new System.Drawing.Size(600, 558);
+            this.picPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picPreview.TabIndex = 37;
+            this.picPreview.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(726, 745);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.btnNightFilter);
+            this.Controls.Add(this.cmbEdgeDetection);
+            this.Controls.Add(this.btnSaveNewImage);
+            this.Controls.Add(this.picPreview);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button btnNightFilter;
+        private System.Windows.Forms.ComboBox cmbEdgeDetection;
+        private System.Windows.Forms.Button btnSaveNewImage;
+        private System.Windows.Forms.PictureBox picPreview;
     }
 }
 
